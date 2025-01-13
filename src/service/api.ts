402c7eaa281cad681e83809e6b0983f7c18c912d
Viktor Stefanov <victorystefanov@gmail.com>
2025-01-13 17:02:24 +0200

    adding api service

---
+++ src/service/api.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { youtube_parser } from "../utils/youtube-parser";
+
+const API_URL = import.meta.env.VITE_API_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+const API_HOST = import.meta.env.VITE_API_HOST;
+
+const request = async (url : string) => {
+
+    const youtubeID = youtube_parser(url);
+
+    const options = {
+        method: 'GET',
+        url: API_URL,
+        headers: {
+          'X-RapidAPI-Key': API_KEY,
+          'X-RapidAPI-Host': API_HOST,
+        },
+        params: {
+          id: youtubeID
+        }
+    };
+
+    const response = await axios(options);
+    
+    return response.data.link;
+};
+
+export default request;
